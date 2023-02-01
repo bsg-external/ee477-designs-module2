337936cce2ece72b2b9be5130548dba71c2a3d33
Scott Davidson <scottdavidson1992@gmail.com>
2023-02-01 02:35:26 -0800

    Adding bsg_chip_timing_constraint prototype to all bsg_guts_* designs

--- bsg_guts_manycore_1x1/cfg/constraints.tcl
+++ bsg_guts_manycore_1x1/cfg/constraints.tcl
@@ -8,7 +8,24 @@
 set CORE_CLOCK_PERIOD      25
 set IO_MASTER_CLOCK_PERIOD 40
 
-# Use pre-built constraints generation script
+# << arguments >>
+# bsg_chip_timing_constraint
+#     [package
+#     [reset_port]
+#     [core_clk_port]
+#     [core_clk_name]
+#     [core_clk_period]
+#     [master_io_clk_port]
+#     [master_io_clk_name]
+#     [master_io_clk_period]
+#     [create_core_clk]
+#     [create_master_clk]
+#     [input_cell_rise_fall_difference]
+#     [output_cell_rise_fall_difference_A]
+#     [output_cell_rise_fall_difference_B]
+#     [output_cell_rise_fall_difference_C]
+#     [output_cell_rise_fall_difference_D]
+
 bsg_chip_timing_constraint    \
     ucsd_bsg_332              \
     [get_ports p_reset_i]     \
@@ -25,3 +42,4 @@ bsg_chip_timing_constraint    \
     0                         \
     0                         \
     0 
+
