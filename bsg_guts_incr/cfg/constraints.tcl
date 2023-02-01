75a906339b34748722f1f83c43b152332c080c9d
Scott Davidson <scottdavidson1992@gmail.com>
2023-02-01 02:28:32 -0800

    Add bsg_chip_timing_constraint prototype

--- bsg_guts_incr/cfg/constraints.tcl
+++ bsg_guts_incr/cfg/constraints.tcl
@@ -8,7 +8,24 @@
 set CORE_CLOCK_PERIOD      20
 set IO_MASTER_CLOCK_PERIOD 20
 
-# Use pre-built constraints generation script
+# << arguments >>
+# bsg_chip_timing_constraint
+#     [package
+#     [reset_port]
+#     [core_clk_port]
+#     [core_clk_name]
+#     [core_clk_period]
+#     [master_io_clk_port]
+#     [master_io_clk_name]
+#     [master_io_clk_period]
+#     [create_core_clk]
+#     [create_master_clk]
+#     [input_cell_rise_fall_difference]
+#     [output_cell_rise_fall_difference_A]
+#     [output_cell_rise_fall_difference_B]
+#     [output_cell_rise_fall_difference_C]
+#     [output_cell_rise_fall_difference_D]
+
 bsg_chip_timing_constraint    \
     ucsd_bsg_332              \
     [get_ports p_reset_i]     \
@@ -26,19 +43,3 @@ bsg_chip_timing_constraint    \
     0                         \
     0 
 
-# bsg_chip_timing_constraint                          \
-#     -package ucsd_bsg_332                           \
-#     -reset_port [get_ports p_reset_i]               \
-#     -core_clk_port [get_ports p_misc_L_4_i]         \
-#     -core_clk_name core_clk                         \
-#     -core_clk_period ${CORE_CLOCK_PERIOD}           \
-#     -master_io_clk_port [get_ports p_PLL_CLK_i]     \
-#     -master_io_clk_name master_io_clk               \
-#     -master_io_clk_period ${IO_MASTER_CLOCK_PERIOD} \
-#     -create_core_clk 1                              \
-#     -create_master_clk 1                            \
-#     -input_cell_rise_fall_difference    0           \
-#     -output_cell_rise_fall_difference_A 0           \
-#     -output_cell_rise_fall_difference_B 0           \
-#     -output_cell_rise_fall_difference_C 0           \
-#     -output_cell_rise_fall_difference_D 0 
